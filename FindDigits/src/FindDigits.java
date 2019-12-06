984a18509b635b62fe0ee52370da1a67eb67834c
Bíró Botond <biro.botond20@gmail.com>
2019-12-06 22:37:01 +0100

    feat(FindDigits): add solution for Find Digits challenge

--- FindDigits/src/FindDigits.java
+++ FindDigits/src/FindDigits.java
@@ -54,30 +54,30 @@ The number 1012 is broken into four digits, 1, 0, 1, and 2. 1012 is evenly divis
 import java.util.Scanner;
 
 public class FindDigits {
-	
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		int testCases = scanner.nextInt();
-		
-		for (int i = 0; i < testCases; i++) {
-			System.out.println(findDigits(scanner.nextInt()));
-		}
-	}
-	
-	private static int findDigits(int n) {
-		int temp = n;
-		int currentDivisor;
-		int numberOfDivisors = 0;
 
-		while (temp != 0) {
-				currentDivisor = temp % 10;
-				if (currentDivisor != 0 && n % currentDivisor == 0) {
-						numberOfDivisors++;
-				}
-				temp = temp / 10;
-		}
+  public static void main(String[] args) {
 
-		return numberOfDivisors;
-	}
+    Scanner scanner = new Scanner(System.in);
+    int testCases = scanner.nextInt();
+
+    for (int i = 0; i < testCases; i++) {
+      System.out.println(findDigits(scanner.nextInt()));
+    }
+  }
+
+  private static int findDigits(int n) {
+    int temp = n;
+    int currentDivisor;
+    int numberOfDivisors = 0;
+
+    while (temp != 0) {
+      currentDivisor = temp % 10;
+      if (currentDivisor != 0 && n % currentDivisor == 0) {
+        numberOfDivisors++;
+      }
+      temp = temp / 10;
+    }
+
+    return numberOfDivisors;
+  }
 }
